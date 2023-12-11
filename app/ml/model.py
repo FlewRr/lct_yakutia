41dc37c9e1f5d9c87edf39456e8dbab1b4f4cb4d
FlewRr <cool.adsdad@mail.ru>
2023-12-11 13:41:16 +0300

    added json prediction

--- app/ml/model.py
+++ app/ml/model.py
@@ -1,14 +1,35 @@
 from catboost import CatBoostClassifier
+import json
+import pandas as pd
+
+TEST_DATA_PATH = "app/ml/data/test.csv"
+JSON_DATA_PATH = "app/ml/data/prediction.json"
 
 class Classifier:
     def __init__(self, model, bin_path):
         self.model = model.load_model(bin_path)
+        self.data_columns = list(pd.read_csv(TEST_DATA_PATH).drop(columns="Вердикт").columns)
 
     def predict(self, data):
+        assert self.data_columns == list(data.columns)
+
         prediction = self.model.predict_proba(data)
         probabilities = [probability[1] * 100 for probability in prediction]
 
-        return probabilities
+        return probabilities, list(data.ID)
     
+    def predict_json(self, data, path):
+        probs, ids = self.predict(data)
 
-model = Classifier("model_bin/model.cbm")
+        probs_dict = {key : value for key, value in zip(ids, probs)}
+        open(path, 'a')
+        with open(path, 'w') as fp:
+            json.dump(probs_dict, fp)
+
+        return probs_dict
+
+# model = Classifier( CatBoostClassifier(), "app/ml/model_bin/model.cbm")
+
+# data = pd.read_csv("app/ml/data/GENERATED_DATASET.csv")[:10].drop(columns='Вердикт')
+
+# print(model.predict_json(data, JSON_DATA_PATH))
