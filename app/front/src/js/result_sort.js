1922245fe2fccd845eecd89dbfa35006df6c1819
Zinkin_A/A <artemij.zinkovich@mail.ru>
2023-12-15 13:05:23 +0300

    StableVersionWithoutCsv

--- app/front/src/js/result_sort.js
+++ app/front/src/js/result_sort.js
@@ -5,9 +5,18 @@ const sortMethods = document.querySelector('.sort-methods');
 const sortArrow = document.querySelector('.sort-arrow');
 const sortType = document.getElementById('sortType');
 
-sortButton.addEventListener('click', () => {
-  sortMethods.classList.toggle('sort-methods-before');
-  sortArrow.classList.toggle('sort-arrow-click');
+sortButton.addEventListener('click', (event) => {
+  event.stopPropagation();
+
+  sortMethods.classList.add('sort-methods-before');
+  sortArrow.classList.add('sort-arrow-click');
+})
+
+document.addEventListener('click', (event) => {
+  if (!sortMethods.contains(event.target)) {
+    sortMethods.classList.remove('sort-methods-before');
+    sortArrow.classList.remove('sort-arrow-click');
+  }
 })
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -56,11 +65,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Добавляем слушатели для кнопок сортировки
   const setMethodButton = document.getElementById('setMethod');
-  setMethodButton.addEventListener('click', sortByPercentDecreasing);
+  setMethodButton.addEventListener('click', () => {
+    sortByPercentDecreasing();
+    sortMethods.classList.remove('sort-methods-before');
+    sortArrow.classList.remove('sort-arrow-click');
+  });
 
 
   const riseMethodButton = document.getElementById('riseMethod');
-  riseMethodButton.addEventListener('click', sortByPercentIncreasing);
+  riseMethodButton.addEventListener('click', () => {
+    sortByPercentIncreasing();
+    sortMethods.classList.remove('sort-methods-before');
+    sortArrow.classList.remove('sort-arrow-click');
+  });
 
   // Инициализация отображения при загрузке
   renderPeople(peopleData);
